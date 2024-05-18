7393b2cfc5177cc57e6fa830bb8f39c8fc4695e3
medzerai <mohamed0zerai@gmail.com>
2024-05-18 18:20:22 +0100

    setup react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">E-commerce App</div>;
+  return <div className="text-red-600">E-commerce App</div>;
 }
 
 export default App;
