065001ee0c5e201276e03b66e4ee9c32d0e4f4f7
medzerai <mohamed0zerai@gmail.com>
2024-05-19 19:02:20 +0100

    add user icons and card

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Logo from "./Logo";
 import { GrSearch } from "react-icons/gr";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -23,7 +25,26 @@ const Header = () => {
           </div>
         </div>
 
-        <div>user icons and card</div>
+        <div className="flex items-center gap-7">
+          <div className="text-3xl cursor-pointer">
+            <FaRegCircleUser />
+          </div>
+
+          <div className="text-2xl relative">
+            <span>
+              <FaShoppingCart />
+            </span>
+            <div className="bg-red-600 text-white w-5 h-5 p-1 rounded-full flex items-center justify-center absolute -top-2 -right-3">
+              <p className="text-sm">0</p>
+            </div>
+          </div>
+
+          <div>
+            <button className="px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700">
+              Login
+            </button>
+          </div>
+        </div>
       </div>
     </header>
   );
