2c961937a883fde2c49d4c051642679f41f23c07
medzerai <mohamed0zerai@gmail.com>
2024-05-19 16:45:00 +0100

    add logo component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Logo from "./Logo";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="h-16 shadow-md">
+      <div className="h-full container mx-auto flex items-center px-4">
+        <div className="">
+          <Logo w={100} h={80} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
