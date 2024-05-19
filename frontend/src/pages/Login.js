69ce343934e85253f3ee9d66141ef69dffeb63cd
medzerai <mohamed0zerai@gmail.com>
2024-05-19 21:37:01 +0100

    add forget password page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import loginIcons from "../assets/signin.gif";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
     <section id="login">
-      <div className="mx-auto container p-4">
+      <div className="mx-auto container p-6">
         <div className="bg-white p-5 w-full max-w-sm mx-auto">
           <div className="w-20 h-20 mx-auto">
             <img src={loginIcons} alt="login icons" srcset="" />
@@ -43,11 +44,27 @@ const Login = () => {
                   <span>{showPassword ? <FaEyeSlash /> : <FaEye />}</span>
                 </div>
               </div>
+
+              <Link
+                to={"/forgot-password"}
+                className="block w-fit ml-auto hover:underline hover:text-red-600"
+              >
+                Forgot password ?
+              </Link>
             </div>
             <button className="bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6">
               Login
             </button>
           </form>
+          <p className="my-5">
+            Don't have account ?{" "}
+            <Link
+              to={"/sign-up"}
+              className=" text-red-600 hover:text-red-700 hover:underline"
+            >
+              Sign up
+            </Link>
+          </p>
         </div>
       </div>
     </section>
