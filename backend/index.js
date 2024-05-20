1f6b286ab8facb23137358195b67e1ae2f4a4120
medzerai <mohamed0zerai@gmail.com>
2024-05-20 13:58:22 +0100

    set up backend and connect to db

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+const PORT = 8080 || process.env.PORT;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("connnect to DB");
+    console.log("Server is running " + PORT);
+  });
+});
