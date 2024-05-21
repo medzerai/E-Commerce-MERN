4471fd949b70087ae1d9ef7605b95387de24e950
medzerai <mohamed0zerai@gmail.com>
2024-05-21 18:18:28 +0100

    setup redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
-import logo from "./logo.svg";
+import logo from "./assets/logo.svg";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
 import SummaryApi from "./common";
-import { useEffect } from "react";
 import Context from "./context";
+import { useDispatch } from "react-redux";
+import { setUserDetails } from "./store/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
   const fetchUserDetails = async () => {
     const dataResponse = await fetch(SummaryApi.current_user.url, {
       method: SummaryApi.current_user.method,
@@ -19,7 +23,7 @@ function App() {
     const dataApi = await dataResponse.json();
 
     if (dataApi.success) {
-      console.log(dataApi.data);
+      dispatch(setUserDetails(dataApi.data));
     }
   };
 
@@ -27,7 +31,6 @@ function App() {
     /**user Details */
     fetchUserDetails();
   }, []);
-
   return (
     <>
       <Context.Provider
@@ -36,6 +39,7 @@ function App() {
         }}
       >
         <ToastContainer position="top-center" />
+
         <Header />
         <main className="min-h-[calc(100vh-120px)] pt-16">
           <Outlet />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </Provider>
 );
 
 reportWebVitals();
