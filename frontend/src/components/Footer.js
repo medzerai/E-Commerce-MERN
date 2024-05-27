2cb299a99a984865cebc8c1d9d69290e55f60a48
medzerai <mohamed0zerai@gmail.com>
2024-05-27 16:11:36 +0100

    fix footer display position

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-200">
+    <footer className="bg-slate-200 fixed bottom-0 w-full">
       <div className="container mx-auto p-4">
-        <p className="text-center font-bold" title="">
+        <p className="text-center font-bold" title="Copyright">
           &copy; Copyright 2024, Baladiya Corporation. All rights reserved
         </p>
       </div>
