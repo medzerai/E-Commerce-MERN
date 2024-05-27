ad0587e2ecf94ce03f19b61b7cf45f0ebfa71d95
medzerai <mohamed0zerai@gmail.com>
2024-05-27 17:17:05 +0100

    add changeUserRole controller and linked

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,7 @@ const userDetailsController = require("../controller/userDetails");
 const authToken = require("../middleware/authToken");
 const userLogout = require("../controller/userLogout");
 const allUsers = require("../controller/allUsers");
+const updateUser = require("../controller/updateUser");
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
@@ -15,5 +16,6 @@ router.get("/user-details", authToken, userDetailsController);
 router.get("/userLogout", userLogout);
 
 router.get("/all-user", authToken, allUsers);
+router.post("/update-user", authToken, updateUser);
 
 module.exports = router;
