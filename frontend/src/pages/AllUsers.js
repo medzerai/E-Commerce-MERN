a5fb87b450dc2196956a1f41ca0953db2db09447
medzerai <mohamed0zerai@gmail.com>
2024-05-27 17:13:19 +0100

    add changeUserRole funtion

--- frontend/src/pages/AllUsers.js
+++ frontend/src/pages/AllUsers.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import SummaryApi from "../common";
 import { toast } from "react-toastify";
+import moment from "moment";
 import { MdModeEdit } from "react-icons/md";
+import ChangeUserRole from "../components/ChangeUserRole";
 
 const AllUsers = () => {
   const [allUser, setAllUsers] = useState([]);
+  const [openUpdateRole, setOpenUpdateRole] = useState(false);
+  const [updateUserDetails, setUpdateUserDetails] = useState({
+    email: "",
+    name: "",
+    role: "",
+    _id: "",
+  });
 
   const fetchAllUsers = async () => {
     const fetchData = await fetch(SummaryApi.allUser.url, {
@@ -48,8 +57,15 @@ const AllUsers = () => {
                 <td>{el?.name}</td>
                 <td>{el?.email}</td>
                 <td>{el?.role}</td>
+                <td>{moment(el?.createdAt).format("LL")}</td>
                 <td>
-                  <button className="bg-green-100 p-2 rounded-full cursor-pointer hover:bg-green-500 hover:text-white">
+                  <button
+                    className="bg-green-100 p-2 rounded-full cursor-pointer hover:bg-green-500 hover:text-white"
+                    onClick={() => {
+                      setUpdateUserDetails(el);
+                      setOpenUpdateRole(true);
+                    }}
+                  >
                     <MdModeEdit />
                   </button>
                 </td>
@@ -58,6 +74,17 @@ const AllUsers = () => {
           })}
         </tbody>
       </table>
+
+      {openUpdateRole && (
+        <ChangeUserRole
+          onClose={() => setOpenUpdateRole(false)}
+          name={updateUserDetails.name}
+          email={updateUserDetails.email}
+          role={updateUserDetails.role}
+          userId={updateUserDetails._id}
+          callFunc={fetchAllUsers}
+        />
+      )}
     </div>
   );
 };
