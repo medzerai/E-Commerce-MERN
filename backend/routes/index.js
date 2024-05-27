2d639a6536e1842b8add7c167d0d7b2e5cd5b2c7
medzerai <mohamed0zerai@gmail.com>
2024-05-27 16:41:49 +0100

    add all users controller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,10 +7,13 @@ const userSignInController = require("../controller/userSignIn");
 const userDetailsController = require("../controller/userDetails");
 const authToken = require("../middleware/authToken");
 const userLogout = require("../controller/userLogout");
+const allUsers = require("../controller/allUsers");
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
 router.get("/user-details", authToken, userDetailsController);
 router.get("/userLogout", userLogout);
+
+router.get("/all-user", authToken, allUsers);
 
 module.exports = router;
