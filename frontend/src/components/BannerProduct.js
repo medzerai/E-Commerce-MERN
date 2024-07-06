f59604e67cdc593bb004e736115d4c103f9538b1
medzerai <mohamed0zerai@gmail.com>
2024-07-06 23:13:09 +0100

    update banners and logo

--- frontend/src/components/BannerProduct.js
+++ frontend/src/components/BannerProduct.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
-import image1 from "../assets/banner/img1.webp";
-import image2 from "../assets/banner/img2.webp";
+import image1 from "../assets/banner/img1.jpg";
+import image2 from "../assets/banner/img2.jpg";
 import image3 from "../assets/banner/img3.jpg";
 import image4 from "../assets/banner/img4.jpg";
-import image5 from "../assets/banner/img5.webp";
+import image5 from "../assets/banner/img5.jpg";
 
-import image1Mobile from "../assets/banner/img1_mobile.jpg";
-import image2Mobile from "../assets/banner/img2_mobile.webp";
-import image3Mobile from "../assets/banner/img3_mobile.jpg";
-import image4Mobile from "../assets/banner/img4_mobile.jpg";
+import image1Mobile from "../assets/banner/img1_mobile.png";
+import image2Mobile from "../assets/banner/img2_mobile.png";
+import image3Mobile from "../assets/banner/img3_mobile.png";
+import image4Mobile from "../assets/banner/img4_mobile.png";
 import image5Mobile from "../assets/banner/img5_mobile.png";
 
 import { FaAngleRight } from "react-icons/fa6";
@@ -53,7 +53,7 @@ const BannerProduct = () => {
 
   return (
     <div className="container mx-auto px-4 rounded ">
-      <div className="h-56 md:h-72 w-full bg-slate-200 relative">
+      <div className="h-100 md:h-75 w-full bg-slate-200 relative">
         <div className="absolute z-10 h-full w-full md:flex items-center hidden ">
           <div className=" flex justify-between w-full text-2xl">
             <button
